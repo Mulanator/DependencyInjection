9a37ef86a58020ac42aaa0f1ea46b53cf0471b10
Jörg Reisslein <joerg@reisslein.de>
2020-09-20 21:21:20 +0200

    Projektdateien hinzufügen.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cw = new ConsoleWriter();
+            var fw = new FileWriter();
+            var cr = new ConsoleReader();
+
+            Copy(cw, cr);
+
+        }
+
+        static void Copy(IWriter writer, IReader reader)
+        {
+            if (writer == null)
+            {
+                throw new ArgumentNullException(nameof(writer));
+            }
+
+            if (reader == null)
+            {
+                throw new ArgumentNullException(nameof(reader));
+            }
+
+            
+            bool machWeiter = true;
+
+            while (machWeiter == true)
+            {
+                var eingabe = reader.Read(true);
+                if (eingabe.Key == ConsoleKey.Escape)
+                {
+                    machWeiter = false;
+                    continue;
+                }
+
+                writer.Write(eingabe.KeyChar);
+            }
+        }
+    }
+}
+
+
+//Konkrete Implementierungen der Abstraktionslayer 
+public class ConsoleWriter : IWriter
+{
+
+    public void Write(char c)
+    {
+        Console.Write(c);
+    }
+}
+
+
+public class FileWriter : IWriter
+{
+
+    public void Write(char c)
+    {
+        //Console.Write(c); write to file by using filestream
+    }
+}
+
+
+public class ConsoleReader : IReader
+{
+    public ConsoleKeyInfo Read(bool intercept)
+    {
+        return Console.ReadKey(intercept);
+    }
+}
+
+
+
+//Abstraktionslayer
+public interface IWriter
+{
+    void Write(char c);
+}
+
+public interface IReader
+{
+    ConsoleKeyInfo Read(bool intercept);
+}
